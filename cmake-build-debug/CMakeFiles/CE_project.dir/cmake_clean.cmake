a1c1bfdd58dda84cfd9bffe57e712d81c2244c05
Amir Iravanimanesh <airavanimanesh@gmail.com>
2021-01-27 15:39:31 +0330

    init

---
+++ cmake-build-debug/CMakeFiles/CE_project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CE_project.exe"
+  "CE_project.exe.manifest"
+  "CE_project.pdb"
+  "CMakeFiles/CE_project.dir/main.c.obj"
+  "libCE_project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CE_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CE_project.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CE_project.
+# This may be replaced when dependencies are built.
